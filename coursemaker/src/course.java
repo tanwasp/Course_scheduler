966c2ead727bb33b998d4a45b9d0645b3b82122a
tanwasp <tanishwasp10@gmail.com>
2023-02-03 13:40:05 -0500

    adding list of courses

--- coursemaker/src/course.java
+++ coursemaker/src/course.java
@@ -1,5 +1,10 @@
 import java.net.StandardSocketOptions;
+import java.sql.SQLOutput;
+import java.util.List;
 import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.util.ArrayList;
 public class Course {
     String courseCode;
     int rating;
@@ -14,18 +19,27 @@ public class Course {
         this.endTime = endTime;
         this.rating = rating;
         this.days = days;
-        this.major = courseCode; // need to use regular expression to get non number part of courseCode
+        this.major = extractNonNumeric(courseCode);
     }
 
     public String toString(){
         return this.courseCode;
+    }
+//    let wrongText = "The sky is silver.";
+//    let silverRegex = /silver/;
+//wrongText.replace(silverRegex, "blue");
+    public static String extractNonNumeric(String input) {
+        Pattern pattern = Pattern.compile("[0-9]");
+        Matcher matcher = pattern.matcher(input);
+        return matcher.replaceAll("");
     }
 
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         Course course1 = new Course("CMPU102", 900, 1015, "MWF", 9);
 
-        Course[] courseList;
+        List<Tuple<Integer, String>> courseList = new ArrayList<>();
+
         String response = "Y";
         while (response.equals("Y")) {
             // input for new course
@@ -40,14 +54,12 @@ public class Course {
                 startTime = in.nextInt();
             }
 
-
             System.out.println("Enter end Time (Ex: \"1015\"): ");
             int endTime = in.nextInt();
             while (endTime < 0 || endTime/100 > 23 || endTime/100 < 9|| endTime%100 > 60 ){
                 System.out.println("Please enter a valid time between 0900 and 2359 hours: ");
                 endTime = in.nextInt();
             }
-
 
             in.nextLine();
             System.out.println("Enter days of the week (Ex: \"MWF\")  (Monday - M, Tuesday - T, Wednesday - W, Thursday - R, Friday - F: ");
@@ -56,12 +68,36 @@ public class Course {
             int rating = in.nextInt();
 
             Course newCourse = new Course(courseCode, startTime, endTime, days, rating);
-            // courseList.add(newCourse);
+            courseList.add(new Tuple<>(newCourse.rating, newCourse.courseCode));
             in.nextLine();
             System.out.println("To enter a course, enter Y/N: ");
             response = in.nextLine();
 
             System.out.println(newCourse);
+            System.out.println("New course major is " + newCourse.major);
+            System.out.println();
+
+            }
+        for (Tuple<Integer, String> t : courseList) {
+            System.out.println(t);
         }
+    }
+}
+
+class Tuple<A, B> {
+    private A first;
+    private B second;
+
+    public Tuple(A first, B second) {
+        this.first = first;
+        this.second = second;
+    }
+
+    public A getFirst() {
+        return first;
+    }
+
+    public B getSecond() {
+        return second;
     }
 }
