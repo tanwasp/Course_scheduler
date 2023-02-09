16bd06ac0d74a45b06f46d91675304ac0a1ad66a
tanwasp <tanishwasp10@gmail.com>
2023-02-09 17:24:57 -0500

    added basic time sorting algorithm

--- coursemaker/src/TupleComparator.java
+++ coursemaker/src/TupleComparator.java
@@ -1,9 +1,9 @@
 import java.util.Comparator;
 
-public class TupleComparator implements Comparator<Tuple<Integer, String>> {
+public class TupleComparator implements Comparator<Tuple<Integer, Course>> {
 //    class for Tuple Comparator
     @Override
-    public int compare(Tuple<Integer, String> t1, Tuple<Integer, String> t2) {
+    public int compare(Tuple<Integer, Course> t1, Tuple<Integer, Course> t2) {
         return t1.getFirst().compareTo(t2.getFirst());
     }
 }
